2fc35ae0b466770a8ed57b5dd3ad964b6beb06eb
joacomacha44 <61840458+joacomacha44@users.noreply.github.com>
2020-11-25 11:34:54 -0300

    Add files via upload

--- ObligatorioP4/main.cpp
+++ ObligatorioP4/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
+#include "DiccionarioVendedores.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Fecha Colon(12,5,1494);
+    Fecha Guerra(5,9,1939);
+    Vendedor * Vend1 = new Zafral(25000, 15, 5088929, "Joaco", Colon, 3000);
+    Vendedor * Vend2 = new Zafral(17800, 3, 5458288, "Juan", Guerra, 4000);
+    Vendedor * Vend3 = new Fijo(1500, 3, 1234345, "Chorizo", 1400);
+    Vendedores diccio;
+    diccio.insert(Vend1);
+    diccio.insert(Vend2);
+    diccio.insert(Vend3);
+    if(diccio.estaVacio())
+        cout << "true" <<endl;
+    else
+        cout << "false" << endl;
+    if(diccio.member(5088929))
+        cout << "true" <<endl;
+    else
+        cout << "false" << endl;
+    Vendedor * v = diccio.find(5088929);
+    cout << v-> getCantVentas() << endl;
+    cout << v-> getCedula() << endl;
+    cout << diccio.calcularmontototaldesueldos() << endl;
+    diccio.registrarcantventas(5088929, 24);
+    cout << "Cantidad de ventas luego: " << v-> getCantVentas() << endl;
+    cout << "Hay " << diccio.contarcuantoszafrales() << " Zafrales" << endl;
+
+
+
 }
